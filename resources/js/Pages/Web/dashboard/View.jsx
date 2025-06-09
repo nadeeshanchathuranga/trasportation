b0dccf8d66044c3c38663c56a5b112edef1c95b7
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2025-06-09 16:14:55 +0530

    fix

--- resources/js/Pages/Web/dashboard/View.jsx
+++ resources/js/Pages/Web/dashboard/View.jsx
@@ -36,16 +36,28 @@ export default function View({ auth }) {
     Booking Summary
   </Link>
     <Link
-    href={route('warehouses.search')}
-    className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-  >
-    <svg className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-    </svg>
-    Find Warehouses
-  </Link>
+        href={route('user.freight_bookings')}
+        className="inline-block text-center bg-purple-600 text-white px-5 py-2 rounded hover:bg-purple-700 transition duration-200"
+    >
+        My Freight Bookings
+    </Link>
+    <Link
+     href={route('warehouses.search')}
+     className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+   >
+     <svg className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+     </svg>
+     Find Warehouses
+   </Link>
+    <Link
+        href={route('freight.booking.create')}
+        className="inline-block text-center bg-green-600 text-white px-5 py-2 rounded hover:bg-green-700 transition duration-200"
+    >
+        New Freight Booking
+    </Link>
 
-  <Link
+      <Link
   href={route('user.airticket_book')}
   className="inline-flex items-center px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700"
 >
@@ -55,6 +67,7 @@ export default function View({ auth }) {
   Air Ticket Book
 </Link>
 </div>
+
 
             </div>
         </AuthenticatedLayout>
