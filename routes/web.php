e5761bbc9700f0be0a75be99513d1b9c658897f5
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2025-07-07 13:50:12 +0530

    front.php route file create

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,14 @@ use Inertia\Inertia;
 // -------------------------------
 // 🌐 Public Routes
 // -------------------------------
-Route::get('/', [WebController::class, 'index'])->name('home');
+
 Route::get('/vehicleList', [WebController::class, 'vehicleList'])->name('vehicle.list');
 Route::get('/vehicleDetails', [WebController::class, 'vehicleDetails'])->name('vehicle.details');
 Route::get('/courier-service', [WebController::class, 'courierService'])->name('courier.service');
 Route::get('/book-a-ticket', [WebController::class, 'bookATicket'])->name('book.a.ticket');
 Route::get('/booking-home', [WebController::class, 'bookingHome'])->name('booking.home');
 Route::get('/cargo-freight', [WebController::class, 'cargoFreight'])->name('cargo.freight');
-Route::get('/drivers-home', [WebController::class, 'driversHome'])->name('drivers.home');
+
 Route::get('/driver-search-results', [WebController::class, 'driverSearchResults'])->name('driver.search.results');
 Route::get('/driver-details', [WebController::class, 'driverDetails'])->name('driver.details');
 
